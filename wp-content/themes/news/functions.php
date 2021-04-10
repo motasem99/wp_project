a164e3d61592706694f7c89f9b506d14d7436605
mutasem <motasemimad10@gmail.com>
2021-04-10 15:04:40 +0300

    clear code

--- wp-content/themes/news/functions.php
+++ wp-content/themes/news/functions.php
@@ -2,15 +2,12 @@
 
 add_theme_support('title-tag');
 
-
-
 function image_sup() {
     add_theme_support('post-thumbnails');
     add_image_size('home_slider', 300, 180, true);
 }
 
-add_action('after_setup_theme', 'image_sup')
-;
+add_action('after_setup_theme', 'image_sup');
 
 function add_css_file() {
     wp_enqueue_style('fontawesome', get_template_directory_uri().'/assets/css/fontawesome.css');
@@ -28,7 +25,6 @@ function add_css_file() {
     wp_enqueue_script('video', get_template_directory_uri().'/assets/js/video.js', array(),'1.5', true);
     wp_enqueue_script('slick-slider', get_template_directory_uri().'/assets/js/slick-slider.js', array(),'1.5', true);
     wp_enqueue_script('custom', get_template_directory_uri().'/assets/js/custom.js', array(),'1.5', true);
-    
 }
 
 add_action('wp_enqueue_scripts', 'add_css_file');
