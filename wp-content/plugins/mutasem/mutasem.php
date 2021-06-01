abfe3f1cf7fbd3e7ee7fbd6bd244fa0429940766
mutasem <motasemimad10@gmail.com>
2021-06-01 18:55:14 +0300

    add custom menu

--- wp-content/plugins/mutasem/mutasem.php
+++ wp-content/plugins/mutasem/mutasem.php
@@ -35,6 +35,29 @@ function print_in_footer() {
 	}}
 }
 
+/**
+ * Register a custom menu page.
+ */
+function wpdocs_register_my_custom_menu_page(){
+    add_menu_page( 
+        __( 'Custom Menu Title', 'textdomain' ),
+        'custom menu',
+        'manage_options',
+        'custompage',
+        'my_custom_menu_page',
+        plugins_url( 'myplugin/images/icon.png' ),
+        6
+    ); 
+}
+add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
+ 
+/**
+ * Display a custom menu page
+ */
+function my_custom_menu_page(){
+    esc_html_e( 'Admin Page Test', 'textdomain' );  
+}
+
 
 function custom_post_type() {
  
@@ -96,3 +119,5 @@ function custom_post_type() {
 	*/
 
 	add_action( 'init', 'custom_post_type', 0 );
+
+
